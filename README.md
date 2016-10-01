d6eddb5c5410ba52a447b1cdf0b3b98c6b7a6f23
Klaudioz <klaudioz@gmail.com>
2016-09-30 20:36:12 -0500

    added README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+Bash scripts for Machine Learning and all of the necessary prerequisites for being used in a AWS g2.2xlarge instance using CUDA.
+
+If you want only Tensorflow with CUDA, you only need to execute scripts: 1,2,3,8
+
+If you install all, probably you will need to increase the free space in the partition
--- script1.sh
+++ script1.sh
@@ -6,12 +6,13 @@ sudo apt-get install -y libopenblas-dev liblapack-dev
 sudo apt-get install -y linux-image-generic linux-image-extra-virtual
 sudo apt-get install -y linux-source linux-headers-generic
 
-sudo touch /etc/modprobe.d/blacklist-nouveau.conf
-sudo echo "blacklist nouveau" >> /etc/modprobe.d/blacklist-nouveau.conf
-sudo echo "blacklist lbm-nouveau" >> /etc/modprobe.d/blacklist-nouveau.conf
-sudo echo "options nouveau modeset=0" >> /etc/modprobe.d/blacklist-nouveau.conf
-sudo echo "alias nouveau off" >> /etc/modprobe.d/blacklist-nouveau.conf
-sudo echo "alias lbm-nouveau off" >> /etc/modprobe.d/blacklist-nouveau.conf
+sudo cat > /etc/modprobe.d/blacklist-nouveau.conf << EOF1
+blacklist nouveau
+blacklist lbm-nouveau
+options nouveau modeset=0
+alias nouveau off
+alias lbm-nouveau off
+EOF1
 
 sudo echo options nouveau modeset=0 | sudo tee -a /etc/modprobe.d/nouveau-kms.conf
 sudo update-initramfs -u
